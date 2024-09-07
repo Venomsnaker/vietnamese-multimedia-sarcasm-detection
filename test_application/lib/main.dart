4470db85828adc199de24768097aafd9480f1722
venomsnaker <qhuy.bui.developer@gmail.com>
2024-09-07 22:32:15 +0700

    Flutter Basic (2)

--- test_application/lib/main.dart
+++ test_application/lib/main.dart
@@ -1,61 +1,107 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
+class Product {
+  const Product({required this.name});
+  final String name;
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+typedef CartChangedCallBack = Function(Product product, bool inCart);
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
+class ShoppingListItem extends StatelessWidget {
+  ShoppingListItem({
+    required this.product,
+    required this.inCart,
+    required this.onCartChanged,
+  }) : super (key: ObjectKey(product));
 
-class _MyAppState extends State<MyApp> {
-  int count = 0;
+  final Product product;
+  final bool inCart;
+  final CartChangedCallBack onCartChanged;
+
+  Color _getColor(BuildContext context) {
+    return inCart ? Colors.black54 : Theme.of(context).primaryColor;
+  }
+
+  TextStyle? _getTextStyle(BuildContext context) {
+    if (!inCart) return null;
+
+    return const TextStyle(
+      color: Colors.black54,
+      decoration: TextDecoration.lineThrough,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.amber,
-          title: const Text("My Flutter journey starts here!"),
+      return ListTile(
+        onTap: () {
+          onCartChanged(product, inCart);
+        },
+        leading: CircleAvatar(
+          backgroundColor: _getColor(context),
+          child: Text(product.name[0]),
         ),
-
-        body:Center(
-          child: Text(
-            '$count'
-          ),
-        ),
-        
-        floatingActionButton: FloatingActionButton(
-          child: const Icon(Icons.add),
-          onPressed: () {
-            setState(() {
-              count++;
-            });
-          }),
-        
-        bottomNavigationBar: BottomNavigationBar(
-          items: const [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Home'),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.business),
-              label: 'Business'),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.school),
-              label: 'School'),
-          ]),
-
-          drawer: const Drawer(
-            child: Text('Yo!')
-          ),
+        title: Text(
+          product.name,
+          style: _getTextStyle(context),
         ),
       );
   }
 }
+
+class ShoppingList extends StatefulWidget {
+  const ShoppingList({required this.products, super.key});
+
+  final List<Product> products;
+
+  @override
+  State<ShoppingList> createState() => _ShoppingListState();
+}
+
+class _ShoppingListState extends State<ShoppingList> {
+  final _shoppingCart = <Product>{};
+
+  void _handleCartChanged(Product product, bool inCart) {
+    setState(() {
+      if (!inCart) {
+        _shoppingCart.add(product);
+      } else {
+        _shoppingCart.remove(product);
+      }
+    });
+  }
+
+  @override
+  Widget build (BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Shopping List"),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(vertical: 8),
+        children: widget.products.map((product) {
+          return ShoppingListItem(
+            product: product,
+            inCart: _shoppingCart.contains(product),
+            onCartChanged: _handleCartChanged,
+          );
+        }).toList(),
+      )
+    );
+  }
+}
+
+void main() {
+  runApp(const MaterialApp(
+    title: "Shopping App",
+    home: ShoppingList(
+      products: [
+        Product(name: "Egg"),
+        Product(name: "Flour"),
+        Product(name: "Chocolate"),
+      ])
+  ));
+}
+
+
+
